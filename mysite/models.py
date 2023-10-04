00fff91c7b641efe77eca35f7db4f1a6ad509e9c
11056031 <11056031@.ntub.edu.tw>
2023-10-04 15:55:58 +0800

    好蒿爽爽

--- mysite/models.py
+++ mysite/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        ordering = ('-pub_date', )
+        ordering = ('-pub_date')
         
     def __str__(self) -> str:
         return self.title
